228320e2f7672daaa4fdeae7c4c65208e321f116
rejnald lleshi <rejnald.lleshi@gmail.com>
2020-09-14 21:55:27 +0200

    update

--- numberbatch_converter.py
+++ numberbatch_converter.py
@@ -1,6 +1,6 @@
 import gensim
 
-PATH = 'numberbatch-en-17.04b.txt'
+PATH = 'files/numberbatch-en-17.04b.txt'
 
 if __name__ == '__main__':
     print('Loading the model...')
@@ -8,9 +8,8 @@ if __name__ == '__main__':
         numberbatch = gensim.models.KeyedVectors.load_word2vec_format(PATH, binary=False, unicode_errors='ignore')
     except FileNotFoundError:
         print('Incorrect file path. Please open the code and modify the PATH variable.')
-        
+
     print('Converting the model...')
     numberbatch.init_sims(replace=True)
     numberbatch.save('conceptNet')
     print('Convertion successful')
-    
